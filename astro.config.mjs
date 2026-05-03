35fb39ee8a68f3706e3c167aa944986e2cd1615d
doctorclo <doctorclo2011@gmail.com>
2026-05-03 13:03:31 +0300

    temp: configure for github.io subpath

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://vikulin.ai',
+  base: '/vikulin-ai',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
